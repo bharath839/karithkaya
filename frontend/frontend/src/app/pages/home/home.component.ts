0e4de3760ebd111d5b0326ad441a5f383edc2ccd
bharath <95297353+bharath839@users.noreply.github.com>
2025-08-26 09:53:10 +0530

    Update home.component.ts

--- frontend/frontend/src/app/pages/home/home.component.ts
+++ frontend/frontend/src/app/pages/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  fullText: string = `
+<h1>Welcome to the Exam Portal!</h1>
+
+<h2>This system helps manage:</h2>
+- Students
+- Exams
+- Results
+- Reports
+we take the Exam and Imporve our knowlege .
+This Application we helps to develop the skils and helps improve our skills and talents .
+For exams its is best choice to prepare.
+Easily track and manage all data in one place.
+  `;
+
+  typedText: string = ""; // will hold the text being typed
+  i: number = 0;          // pointer to current character
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.typingEffect();
   }
 
+  typingEffect() {
+    if (this.i < this.fullText.length) {
+      this.typedText += this.fullText.charAt(this.i);
+      this.i++;
+      setTimeout(() => this.typingEffect(), 50); // typing speed
+    }
+  }
 }
